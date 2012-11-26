8a4b73ec7e4932d8f751d3f048a5f0d1bf871182
Philippe Pittoli <kane.root@gmail.com>
2012-11-26 11:49:38 +0100

    Gestion de l'ancienne frame

--- kitties/Computation.java
+++ kitties/Computation.java
@@ -1,11 +1,11 @@
 public class Computation
 {
-	private KittyCluster cluster;
+	private KittyCluster cluster, oldCluster;
 	private KittyHistory kittyhistory;
 
 	public Computation(KittyCluster c)
 	{
-		this.cluster = c;
+		this.cluster = c.getCopy();
 		this.kittyhistory = new KittyHistory();
 	}
 
@@ -15,15 +15,18 @@ public class Computation
 		int end_columns;
 		int end_line;
 
-		for(start = 1 ; start < 16 ; start++)
+		for(start = 1 ; start < 17 ; start++)
 		{
-			end_columns = (cluster.getNbColumns()) - start;
-			end_line = (cluster.getNbLines()) - start;
+			this.oldCluster = this.cluster.getCopy();
+			this.oldCluster.displayKittyCluster();
+			end_columns = (oldCluster.getNbColumns()) - start;
+			end_line = (oldCluster.getNbLines()) - start;
 
+			System.out.println("NbLignes : " + end_line + " nbcolonnes : " + end_columns);
 			for(int line = start ; line < end_line ; line++)
 				for(int column = start ; column < end_columns ; column++)
 				{
-					if(cluster.isKittyAlive(line, column))
+					if(oldCluster.isKittyAlive(line, column))
 					{
 						if(shouldKittyDie(line, column))
 							cluster.killKitty(line, column);
@@ -31,19 +34,18 @@ public class Computation
 					else if(shouldKittyRaise(line, column))
 						cluster.raiseKitty(line, column);
 				}
-			this.kittyhistory.addSnapshot(this.cluster.getCopy());
-			this.cluster.displayKittyCluster();
+			this.kittyhistory.addSnapshot(this.oldCluster.getCopy());
 			try { java.lang.Thread.sleep(1000); } catch(Exception e) {}
 		}
 	}
 
 	private int nbKittiesLivingAround(int line, int column)
 	{
-		int count = cluster.isKittyAlive(line, column) ? -1 : 0;
+		int count = oldCluster.isKittyAlive(line, column) ? -1 : 0;
 
 		for(int l = -1 ; l < 2 ; l++)
-			for(int i = -1 ; i < 2 ; i++)
-				count = cluster.isKittyAlive(line + l, column + i) ? count + 1 : count;
+			for(int c = -1 ; c < 2 ; c++)
+				count = oldCluster.isKittyAlive(line + l, column + c) ? count + 1 : count;
 
 		return count;
 	}
@@ -88,7 +90,6 @@ public class Computation
 		for(KittyCluster kc : this.kittyhistory.getHistory())
 		{
 			System.out.println("## " + k++ + " ##");
-			//System.out.println("NbLignes : " + kc.getNbLines() + " nbcolonnes : " + kc.getNbColumns());
 			kc.displayKittyCluster();
 		}
 	}
