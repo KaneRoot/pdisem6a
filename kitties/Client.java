69d9a61b4bab48b67db1676f797666eb9d2caa38
Karchnu <kane.root@gmail.com>
2012-12-01 18:50:53 +0100

    Un client qui fait des choses

--- kitties/Client.java
+++ kitties/Client.java
@@ -4,10 +4,13 @@ import java.net.* ;
 
 public class Client
 {
+	public static int TIME_TO_WAIT = 1000;
 	public static void main(String[] args)
 	{
 		int licenceToKill = -1;
 		KittyPimp datKittyPimp = null;
+		KittyCluster cluster = null;
+		Computation compute;
 
 		if(args.length != 2)
 		{
@@ -20,6 +23,12 @@ public class Client
 			datKittyPimp = (KittyPimp) Naming.lookup("rmi://"+args[0]+":"+args[1]+"/KittyPimp") ;
 			licenceToKill = datKittyPimp.gimmeLicenseToKill();
 			System.out.println("My licence to kill kitties : " + licenceToKill);
+			cluster = datKittyPimp.gimmeKittiesToKill(licenceToKill);
+			System.out.println("Computation");
+			compute = new Computation(cluster);
+			compute.kitty_life_game(32);
+			System.out.println("Send result of the genocide");
+			datKittyPimp.resultsOfTheGenocide( compute.getHistory(), licenceToKill);
 		}
 		catch (Exception e)
 		{
